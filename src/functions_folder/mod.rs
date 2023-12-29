cb773284f076594d3c8e34c317da404a6ad9c3c7
Joseg-05 <49496262+Joseg-05@users.noreply.github.com>
2023-12-29 13:40:14 -0800

    Add some comments
    Added some comments to my code

--- src/functions_folder/mod.rs
+++ src/functions_folder/mod.rs
@@ -2,6 +2,10 @@ mod structs_folder;
 use structs_folder::my_car_struct::Car;
 use std::io;
 use std::io::Write;
+/*
+    This is my first function and it involves creating a Struct from another module
+    This function also creates a string and prints out some words to the console
+ */
 pub fn my_first_module_function() -> () {
     let car: Car =  Car { color: String::from("Green"), make: String::from("Toyota") };
     println!("The color of my car is: {}, and the make is: {}", car.color, car.make);
@@ -9,7 +13,9 @@ pub fn my_first_module_function() -> () {
     println!("{}", tommy);
     println!("Hello, world!");
 }
-
+/*
+    This functions grabs the user details and tells the age of the bot
+*/
 pub fn getting_user_details() -> () {
 
     let name_of_bot = "Rusty";
