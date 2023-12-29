55fff88be383a54a37a8ff83cd09b5077fcc1a93
Joseg-05 <49496262+Joseg-05@users.noreply.github.com>
2023-12-29 15:20:20 -0800

    Update my comments
    Update my comments so that the user can understand what is going on

--- src/functions_folder/mod.rs
+++ src/functions_folder/mod.rs
@@ -6,7 +6,7 @@ use std::io;
 use std::io::Write;
 
 /*
-    This is my first function and it involves creating a Struct from another module
+    This is my first function and it involves creating a Struct imported from another module
     This function also creates a string and prints out some words to the console
  */
 pub fn my_first_module_function() -> () {
@@ -17,7 +17,7 @@ pub fn my_first_module_function() -> () {
     println!("Hello, world!");
 }
 /*
-    This functions grabs the user details and tells the age of the bot
+    This functions creates a bot and grabs the user details
 */
 pub fn getting_user_details() -> () {
 
@@ -36,14 +36,17 @@ pub fn getting_user_details() -> () {
     print!("Hi {0}, \nnice to meet you {0}!!!! \n\n", name.trim_end());
 }
 /*
-    This is to use an enum, random case
+    This functions initializes a varialbe with an Enum type Weather
+    Inside this function is called another function to proceed to a given match case
 */
 pub fn using_enums() -> () {
     let rainy: Weather = Weather::Rainy;
 
     math_base_given_enum(rainy);
 }
-
+/*
+    This functino receives a type of Weather and does pattern matching based on the data it receives
+*/
 pub fn math_base_given_enum(given: Weather) -> () {
     match given {
         Weather::Rainy => print!("Today weather is rainy \n\n"),
