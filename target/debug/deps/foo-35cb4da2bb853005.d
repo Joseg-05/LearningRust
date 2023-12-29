f255952a240ad2deae6db6cfe847b13ff1a4b591
Joseg-05 <49496262+Joseg-05@users.noreply.github.com>
2023-12-29 13:09:36 -0800

    another update

--- target/debug/deps/foo-35cb4da2bb853005.d
+++ target/debug/deps/foo-35cb4da2bb853005.d
@@ -1,6 +1,6 @@
-/Users/josegarcia/Desktop/rustproject/foo/target/debug/deps/foo-35cb4da2bb853005: src/main.rs src/functions_folder/mod.rs src/functions_folder/structs_folder/mod.rs src/functions_folder/structs_folder/my_car_struct.rs
+/Users/josegarcia/Documents/GitHub/LearningRust/target/debug/deps/foo-35cb4da2bb853005: src/main.rs src/functions_folder/mod.rs src/functions_folder/structs_folder/mod.rs src/functions_folder/structs_folder/my_car_struct.rs
 
-/Users/josegarcia/Desktop/rustproject/foo/target/debug/deps/foo-35cb4da2bb853005.d: src/main.rs src/functions_folder/mod.rs src/functions_folder/structs_folder/mod.rs src/functions_folder/structs_folder/my_car_struct.rs
+/Users/josegarcia/Documents/GitHub/LearningRust/target/debug/deps/foo-35cb4da2bb853005.d: src/main.rs src/functions_folder/mod.rs src/functions_folder/structs_folder/mod.rs src/functions_folder/structs_folder/my_car_struct.rs
 
 src/main.rs:
 src/functions_folder/mod.rs:
